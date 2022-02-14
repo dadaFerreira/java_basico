42fa4e61eec194c429544da2452ce63bd05e1334
Anderson Ferreira <98371064+dadaFerreira@users.noreply.github.com>
2022-02-14 20:15:43 -0300

    Add files via upload

---
+++ VariaveisChar.java
@@ -0,0 +1,18 @@
+package com.loiane.cursojava.aula11;
+
+public class VariaveisChar {
+
+	public static void main(String[] args) {
+		
+		//char o = 'o';
+		//char i = 'i';
+		
+		char o = 111;
+		char i = 105;
+		char interrogacao = 00111111; // valor = '?'
+		
+		System.out.println(""+o+i+interrogacao);
+
+	}
+
+}
---
+++ variaveis.java
@@ -0,0 +1,21 @@
+package com.loiane.cursojava.aula10;
+
+public class variaveis {
+
+	public static void main(String[] args) {
+		int idade = 20;
+		String nome = "Loiane";
+		String nomeDoMeuCachorro = "totó";
+		String ano2022 = "2022";
+		idade = 25;
+		
+		System.out.println("Idade= "+ idade);
+		System.out.println("Nome= "+ nome);
+		System.out.println("Ano= "+ ano2022);
+		// má prática
+		int a = 10;
+		String b = "Loiane";
+	}
+
+}
+ 
